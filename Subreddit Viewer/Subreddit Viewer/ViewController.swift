3443f08a5653b6933befe409d780ee1b1a76dd46
joewhite <jwwhite237@gmail.com>
2018-04-26 19:26:31 +0100

    Initial Commit
    Generated Xcode project and added code to parse JSON from URL.

---
+++ Subreddit Viewer/Subreddit Viewer/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Subreddit Viewer
+//
+//  Created by Joseph White on 26/04/2018.
+//  Copyright © 2018 Netsells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://www.reddit.com/r/ios/hot.json")!
+        
+        URLSession.shared.dataTask(with: url) { data, _, _ in
+            guard let data = data else { return }
+            guard let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) else { return }
+            guard let jsonObj = json as? [String: Any] else {return}
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
